1a113fa644ca59762b7e05fd6a3f41e7fc2eedaf
Mounish Vatti <mounishvatti2002@gmail.com>
2025-03-03 18:10:08 +0530

    monorepo structure

--- frontend/src/sections/header.tsx
+++ frontend/src/sections/header.tsx
@@ -4,7 +4,7 @@ export default function Header() {
   return (
     <header className="dark:bg-secondaryBlack inset-0 flex min-h-[80dvh] w-full flex-col items-center justify-center bg-white bg-[linear-gradient(to_right,#80808033_1px,transparent_1px),linear-gradient(to_bottom,#80808033_1px,transparent_1px)] bg-[size:70px_70px]">
       <div className="mx-auto w-full px-5 py-[110px] text-center lg:py-[150px]">
-        <h1 className="text-3xl font-heading md:text-4xl lg:text-5xl">
+        <h1 className="text-5xl font-heading md:text-4xl lg:text-5xl">
           GuessQuest
         </h1>
         <p className="my-12 mt-8 text-lg font-normal leading-relaxed md:text-xl lg:text-2xl lg:leading-relaxed">
