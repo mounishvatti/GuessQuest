8ecff41d413595b621c3976f82426dc66cf58e9b
Mounish Vatti <vatti.mounish@gmail.com>
2026-04-24 21:31:52 +0530

    change prisma config

--- backend/src/db.ts
+++ backend/src/db.ts
@@ -1,5 +1,12 @@
-import { PrismaClient } from '@prisma/client';
+import { PrismaClient } from '@prisma/client'
+import { PrismaNeon } from '@prisma/adapter-neon'
 
-const prisma = new PrismaClient();
+const connectionString = process.env.DATABASE_URL
 
-export default prisma;
+const adapter = new PrismaNeon({
+  connectionString,
+})
+
+const prisma = new PrismaClient({ adapter })
+
+export default prisma
